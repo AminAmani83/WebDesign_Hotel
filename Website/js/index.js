b8c5aefffbc6ddf847322aafbc2a8502eee5aafb
Amin Amani <Amin_Amani@hotmail.com>
2019-11-06 12:23:31 -0500

    Changes After Submission

--- Website/js/index.js
+++ Website/js/index.js
@@ -1,37 +1,33 @@
-console.log("index.js has loaded");
 
-var checkBtns = document.getElementsByClassName('check');
-for(var i = 0; i < checkBtns.length; i++){
-    checkBtns[i].addEventListener("click", showResults);
-}
+// Used in Index.html ////////////////////////////////
+
+$("#check-button-top").click(showResults);
+$("#check-button-bottom").click(showResults);
 
 function showResults(e){
     e.preventDefault();
-    var checkIn = document.getElementById('date-from-top').value;
-    var checkOut = document.getElementById('date-to-top').value;
-    if (checkIn != "" && checkOut !== "") {
-        window.location.replace("search_result.html");
-        console.log('we are here');
-    } else{
+    // Was it the top form or the bottom form button that was clicked?
+    let whichForm = (this.id == "check-button-top") ? "top" : "bottom";
+
+    let checkIn = $("#date-from-" + whichForm).val();
+    let checkOut = $("#date-to-" + whichForm).val();
+    
+    if ( checkIn && checkOut ) { // Validation Pass
+        window.location.href = "search_result.html"; // go to page
+    } else { // Validation Fail
         alert("Please choose check-in and check-out dates");
     }
 }
 
-var Bookbtns = document.getElementsByClassName('book');
-for(var i = 0; i < Bookbtns.length; i++){
-    checkBtns[i].addEventListener("click", redirectToBook);
+
+// Used in Booking.html ////////////////////////////////
+
+let Bookbtns = $(".book");
+for(let i=0 ; i<Bookbtns.length ; i++){
+    Bookbtns[i].addEventListener("click", redirectToBook);
 }
 
 function redirectToBook(e){
     e.preventDefault();
-    window.location.replace("search_result.html");
-}
-
-
-// document.getElementById("check-availability-top").addEventListener("click", openSearchresults);
-
-
-function openSearchresults(e){
-    e.preventDefault();
-    window.location.replace("search_result.html");
+    window.location.href = "booking.html";
 }
